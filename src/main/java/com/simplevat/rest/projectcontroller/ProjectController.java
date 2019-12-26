81792552d44ec944779bf791ccbd93df2338a39f
Uday Kumar <udaykumar.mca@gmail.com>
2019-12-27 01:04:44 +0530

    fileUpload functionality Implemented

--- src/main/java/com/simplevat/rest/projectcontroller/ProjectController.java
+++ src/main/java/com/simplevat/rest/projectcontroller/ProjectController.java
@@ -6,17 +6,14 @@
 package com.simplevat.rest.projectcontroller;
 
 import com.simplevat.bank.model.DeleteModel;
-import com.simplevat.constant.dbfilter.ProductFilterEnum;
 import com.simplevat.constant.dbfilter.ProjectFilterEnum;
 import com.simplevat.criteria.ProjectCriteria;
 import com.simplevat.entity.Contact;
 import com.simplevat.entity.Country;
 import com.simplevat.entity.Currency;
-import com.simplevat.entity.Product;
 import com.simplevat.entity.Project;
 import com.simplevat.entity.Title;
 import com.simplevat.rest.contactController.ContactHelper;
-import com.simplevat.rest.productcontroller.ProductRequestFilterModel;
 import com.simplevat.service.ContactService;
 import com.simplevat.service.CountryService;
 import com.simplevat.service.CurrencyService;
@@ -26,6 +23,7 @@ import com.simplevat.service.TitleService;
 import io.swagger.annotations.ApiOperation;
 
 import com.simplevat.contact.model.ContactModel;
+import com.simplevat.security.JwtTokenUtil;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -51,176 +49,81 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  *
  * @author Sonu
- * 
+ *
  * Modified by saurabh 26/12/19
  */
 @RestController
 @RequestMapping(value = "/rest/project")
 public class ProjectController implements Serializable {
 
-	@Autowired
-	private ProjectService projectService;
+    @Autowired
+    private ProjectService projectService;
 
-	@Autowired
-	private ProjectRestHelper projectRestHelper;
+    @Autowired
+    private ProjectRestHelper projectRestHelper;
 
-	@Autowired
-	private CurrencyService currencyService;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
 
-	@Autowired
-	private ContactService contactService;
+    @ApiOperation(value = "Get Project List")
+    @PostMapping(value = "/getList")
+    public ResponseEntity getProductList(ProjectRequestFilterModel filterModel, HttpServletRequest request) {
+        Map<ProjectFilterEnum, Object> filterDataMap = new HashMap();
+        filterDataMap.put(ProjectFilterEnum.USER_ID, filterModel.getUserId());
+        filterDataMap.put(ProjectFilterEnum.PROJECT_ID, filterModel.getProductId());
+        filterDataMap.put(ProjectFilterEnum.PROJECT_NAME, filterModel.getProjectName());
+        filterDataMap.put(ProjectFilterEnum.DELETE_FLAG, filterModel.isDeleteFlag());
+        List<Project> products = projectService.getProjectList(filterDataMap);
+        if (products == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity(projectRestHelper.getListModel(products), HttpStatus.OK);
+    }
 
-	@Autowired
-	private TitleService titleService;
+    @ApiOperation(value = "Delete Project By ID")
+    @DeleteMapping(value = "/delete")
+    public ResponseEntity deleteProject(@RequestParam(value = "id") Integer id) throws Exception {
+        try {
+            Project project = projectService.findByPK(id);
 
-	@Autowired
-	private CountryService countryService;
+            if (project == null) {
+                return new ResponseEntity(HttpStatus.NOT_FOUND);
+            } else {
+                project.setDeleteFlag(Boolean.TRUE);
+                projectService.update(project);
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
-	@ApiOperation(value = "Get Project List")
-	@PostMapping(value = "/getList")
-	public ResponseEntity getProductList(ProjectRequestFilterModel filterModel, HttpServletRequest request) {
-		Map<ProjectFilterEnum, Object> filterDataMap = new HashMap();
-		filterDataMap.put(ProjectFilterEnum.USER_ID, filterModel.getUserId());
-		filterDataMap.put(ProjectFilterEnum.PROJECT_ID, filterModel.getProductId());
-		filterDataMap.put(ProjectFilterEnum.PROJECT_NAME, filterModel.getProjectName());
-		filterDataMap.put(ProjectFilterEnum.DELETE_FLAG, filterModel.isDeleteFlag());
-		List<Project> products = projectService.getProjectList(filterDataMap);
-		if (products == null) {
-			return new ResponseEntity(HttpStatus.NOT_FOUND);
-		}
-		return new ResponseEntity(projectRestHelper.getListModel(products), HttpStatus.OK);
-	}
+    @ApiOperation(value = "Delete Project in Bulk")
+    @DeleteMapping(value = "/deletes")
+    public ResponseEntity deleteProjects(@RequestBody DeleteModel ids) throws Exception {
+        try {
+            projectService.deleteByIds(ids.getIds());
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
-	@ApiOperation(value = "Delete Product By ID")
-	@DeleteMapping(value = "/delete")
-	public ResponseEntity deleteProject(@RequestParam(value = "id") Integer id) throws Exception {
-		try {
-			Project project = projectService.findByPK(id);
-
-			if (project == null) {
-				return new ResponseEntity(HttpStatus.NOT_FOUND);
-
-			} else {
-				project.setDeleteFlag(Boolean.TRUE);
-				projectService.update(project);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return new ResponseEntity<>(HttpStatus.OK);
-	}
-
-	@ApiOperation(value = "Delete Product in Bulk")
-	@DeleteMapping(value = "/deletes")
-	public ResponseEntity deleteProjects(@RequestBody DeleteModel ids) throws Exception {
-		try {
-			projectService.deleteByIds(ids.getIds());
-			return new ResponseEntity<>(HttpStatus.OK);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
-	}
-
-	
-	
-	@Deprecated
-	@GetMapping(value = "/getprojects")
-	public ResponseEntity getProjects() throws Exception {
-		ProjectCriteria projectCriteria = new ProjectCriteria();
-		projectCriteria.setActive(Boolean.TRUE);
-		List<Project> projects = projectService.getProjectsByCriteria(projectCriteria);
-
-		if (projects == null) {
-			return new ResponseEntity(HttpStatus.NOT_FOUND);
-		}
-		return new ResponseEntity<>(projects, HttpStatus.OK);
-	}
-
-	@Deprecated
-	@GetMapping(value = "/getcurrenncy")
-	public ResponseEntity getCurrency() {
-		try {
-			List<Currency> currencies = currencyService.getCurrencies();
-			if (currencies != null && !currencies.isEmpty()) {
-				return new ResponseEntity<>(currencies, HttpStatus.OK);
-			} else {
-				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-
-	}
-
-	@GetMapping(value = "/gettitle")
-	public ResponseEntity getTitle(@RequestParam(value = "titleStr") String titleStr) {
-		List<Title> titleSuggestion = new ArrayList<>();
-		List<Title> titles = titleService.getTitles();
-		Iterator<Title> titleIterator = titles.iterator();
-		if (titleIterator == null) {
-			return new ResponseEntity(HttpStatus.NOT_FOUND);
-
-		} else {
-			while (titleIterator.hasNext()) {
-				Title title = titleIterator.next();
-				if (title.getTitleDescription() != null && !title.getTitleDescription().isEmpty()
-						&& title.getTitleDescription().toUpperCase().contains(titleStr.toUpperCase())) {
-					titleSuggestion.add(title);
-				}
-			}
-
-			return new ResponseEntity<>(titleSuggestion, HttpStatus.OK);
-		}
-	}
-
-	@Deprecated
-	@GetMapping(value = "/getcountry")
-	public ResponseEntity getCountry() {
-		try {
-			List<Country> countries = countryService.getCountries();
-			if (countries != null && !countries.isEmpty()) {
-				return new ResponseEntity<>(countries, HttpStatus.OK);
-			} else {
-				return new ResponseEntity(HttpStatus.NOT_FOUND);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
-
-	}
-
-	@PostMapping(value = "/saveprojectcontact")
-	public ResponseEntity saveContact(@RequestBody ContactModel contactModel, @RequestParam(value = "id") Integer id) {
-
-		Contact contact = new Contact();
-		ContactHelper contactHelper = new ContactHelper();
-		contact = contactHelper.getContact(contactModel);
-		contact.setCreatedBy(id);
-		contact.setCreatedDate(LocalDateTime.now());
-		contact.setDeleteFlag(Boolean.FALSE);
-		if (contact.getContactId() != null && contact.getContactId() > 0) {
-			this.contactService.update(contact);
-		} else {
-
-			this.contactService.persist(contact);
-		}
-		return new ResponseEntity<>(HttpStatus.OK);
-	}
-
-	@PostMapping(value = "/saveproject")
-	public ResponseEntity saveProject(@RequestBody Project project, @RequestParam(value = "id") Integer id)
-			throws Exception {
-		project.setCreatedBy(id);
-		project.setCreatedDate(LocalDateTime.now());
-		if (project.getProjectId() != null && project.getProjectId() > 0) {
-			projectService.update(project);
-		} else {
-			projectService.persist(project);
-		}
-		return new ResponseEntity<>(HttpStatus.OK);
-
-	}
+    @PostMapping(value = "/save")
+    public ResponseEntity saveProject(@RequestBody Project project, HttpServletRequest request)
+            throws Exception {
+        Integer userId = jwtTokenUtil.getUserIdFromHttpRequest(request);
+        if (project.getProjectId() != null && project.getProjectId() > 0) {
+            project.setLastUpdateBy(userId);
+            project.setLastUpdateDate(LocalDateTime.now());
+            projectService.update(project);
+        } else {
+            project.setCreatedBy(userId);
+            project.setCreatedDate(LocalDateTime.now());
+            projectService.persist(project);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }

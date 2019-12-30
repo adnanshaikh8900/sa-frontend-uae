4f01c04d6e3c0aaf17323716045e6328f6970c00
Rohini Kuperkar <rohini.kuperkar@daynilgroup.com>
2019-12-30 19:19:40 +0530

    Expense and Payment Filter code added for Frontend

--- frontend/src/screens/payment/sections/supplier_modal.js
+++ frontend/src/screens/payment/sections/supplier_modal.js
@@ -8,7 +8,7 @@ import {
   Input,
   Label,
   Modal,
-  ModalHeader, 
+  ModalHeader,
   ModalBody,
   ModalFooter,
 } from 'reactstrap'
@@ -18,138 +18,162 @@ import _ from 'lodash'
 import { Formik } from 'formik';
 import * as Yup from "yup";
 
-import * as ProjectActions from '../actions'
-import {selectOptionsFactory} from 'utils'
+import { selectOptionsFactory } from 'utils'
+import { ToastContainer, toast } from 'react-toastify'
+
 
 
 class SupplierModal extends React.Component {
-  
+
   constructor(props) {
     super(props)
     this.state = {
       loading: false,
-
       initValue: {
         firstName: '',
         lastName: '',
         middleName: '',
-        contactType: 'supplier'
+        contactType: 1
       },
     }
-
+    this.formikRef = React.createRef()
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.displayMsg = this.displayMsg.bind(this)
   }
 
   // Create or Contact
-  handleSubmit(data) {
-
-    const request = this.props.createSupplier(data);
-    request.then(res => {
+  handleSubmit(data,resetForm,setSubmitting) {
+    this.props.createSupplier(data).then(res => {
       if (res.status === 200) {
-      // this.success()
-      this.closeContactModel()
+        resetForm();
+        const val = res.data
+        this.props.closeSupplierModal(true)
+        this.props.getCurrentUser(res.data)
       }
+    }).catch(err => {
+      this.displayMsg();
+      this.formikRef.current.setSubmitting(false);
+    })
+  }
+
+  displayMsg() {
+    toast.error('Something Went Wrong... ', {
+      position: toast.POSITION.TOP_RIGHT
     })
   }
 
   render() {
-    const { openSupplierModal, closeSupplierModal} = this.props
+    const { openSupplierModal, closeSupplierModal } = this.props
+    const { initValue } = this.state
     return (
       <div className="contact-modal-screen">
         <Modal isOpen={openSupplierModal}
           className="modal-success contact-modal"
-          >
+        >
           <Formik
-            initialValues={this.state.initValue}
-            onSubmit={(values, {resetForm}) => {
-              this.handleSubmit(values)
-              resetForm(this.state.initValue)
+            ref={this.formikRef}
+            initialValues={initValue}
+            onSubmit={(values, { resetForm , setSubmitting}) => {
+              this.handleSubmit(values,resetForm)
             }}
-            >
-            {props => (
+            validationSchema={
+              Yup.object().shape({
+                firstName: Yup.string()
+                  .required("FirstName is Required"),
+                lastName: Yup.string()
+                  .required("LastName is Required"),
+                middleName: Yup.string()
+                  .required("MiddleName is Required")
+              })
+            }
+          >
+            {props => {
+                const { isSubmitting } = props;
+              return (
               <Form name="simpleForm" onSubmit={props.handleSubmit}>
                 <ModalHeader toggle={this.toggleDanger}>New Supplier</ModalHeader>
-                  <ModalBody>
+                <ModalBody>
                   <Row>
-                      <Col>
-                        <FormGroup>
-                          <Label htmlFor="firstName"><span className="text-danger">*</span>First Name</Label>
-                            <Input
-                              type="text"
-                              id="firstName"
-                              name="firstName"
-                              onChange={props.handleChange}
-                              placeholder="Enter FirstName "
-                              value={props.values.firstName}
-                              // className={
-                              //   props.errors.warehouseName  && props.touched.warehouseName 
-                              //   ? "is-invalid"
-                              //   : ""
-                              // }
-                            />
-                          {/* {props.errors.warehouseName  && props.touched.warehouseName  && (
-                            <div className="invalid-feedback">{props.errors.warehouseName }</div>
-                          )} */}
-                        </FormGroup>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col>
-                        <FormGroup>
-                          <Label htmlFor="lastName"><span className="text-danger">*</span>Middle Name</Label>
-                            <Input
-                              type="text"
-                              id="middleName"
-                              name="middleName"
-                              onChange={props.handleChange}
-                              placeholder="Enter Middle Name "
-                              value={props.values.middleName}
-                              // className={
-                              //   props.errors.warehouseName  && props.touched.warehouseName 
-                              //   ? "is-invalid"
-                              //   : ""
-                              // }
-                            />
-                          {/* {props.errors.warehouseName  && props.touched.warehouseName  && (
-                            <div className="invalid-feedback">{props.errors.warehouseName }</div>
-                          )} */}
-                        </FormGroup>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col>
-                        <FormGroup>
-                          <Label htmlFor="lastName"><span className="text-danger">*</span>Last Name</Label>
-                            <Input
-                              type="text"
-                              id="lastName"
-                              name="lastName"
-                              onChange={props.handleChange}
-                              placeholder="Enter Last Name "
-                              value={props.values.lastName}
-                              // className={
-                              //   props.errors.warehouseName  && props.touched.warehouseName 
-                              //   ? "is-invalid"
-                              //   : ""
-                              // }
-                            />
-                          {/* {props.errors.warehouseName  && props.touched.warehouseName  && (
-                            <div className="invalid-feedback">{props.errors.warehouseName }</div>
-                          )} */}
-                        </FormGroup>
-                      </Col>
-                    </Row>
+                    <Col>
+                      <FormGroup>
+                        <Label htmlFor="firstName"><span className="text-danger">*</span>First Name</Label>
+                        <Input
+                          type="text"
+                          id="firstName"
+                          name="firstName"
+                          onChange={props.handleChange}
+                          placeholder="Enter FirstName "
+                          value={props.values.firstName}
+                          className={
+                            props.errors.firstName && props.touched.firstName
+                              ? "is-invalid"
+                              : ""
+                          }
+                        />
+                        {props.errors.firstName && props.touched.firstName && (
+                          <div className="invalid-feedback">{props.errors.firstName}</div>
+                        )}
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <FormGroup>
+                        <Label htmlFor="lastName"><span className="text-danger">*</span>Middle Name</Label>
+                        <Input
+                          type="text"
+                          id="middleName"
+                          name="middleName"
+                          onChange={props.handleChange}
+                          placeholder="Enter Middle Name "
+                          value={props.values.middleName}
+                          className={
+                            props.errors.middleName && props.touched.middleName
+                              ? "is-invalid"
+                              : ""
+                          }
+                        />
+                        {props.errors.middleName && props.touched.middleName && (
+                          <div className="invalid-feedback">{props.errors.middleName}</div>
+                        )}
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <FormGroup>
+                        <Label htmlFor="lastName"><span className="text-danger">*</span>Last Name</Label>
+                        <Input
+                          type="text"
+                          id="lastName"
+                          name="lastName"
+                          onChange={props.handleChange}
+                          placeholder="Enter Last Name "
+                          value={props.values.lastName}
+                          className={
+                            props.errors.lastName && props.touched.lastName
+                              ? "is-invalid"
+                              : ""
+                          }
+                        />
+                        {props.errors.lastName && props.touched.lastName && (
+                          <div className="invalid-feedback">{props.errors.lastName}</div>
+                        )}
+                      </FormGroup>
+                    </Col>
+                  </Row>
 
-                  </ModalBody>
-                  <ModalFooter>
-                    <Button color="success" type="submit" className="btn-square">Save</Button>&nbsp;
-                    <Button color="secondary" className="btn-square" onClick={closeSupplierModal}>Cancel</Button>
-                  </ModalFooter>
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="success" type="submit" className="btn-square" disabled={isSubmitting}>Save</Button>&nbsp;
+                    <Button color="secondary" className="btn-square" onClick={()=>{closeSupplierModal(false)}}>Cancel</Button>
+                </ModalFooter>
               </Form>
-              )}
-            </Formik>
-          </Modal>
-        </div>
+            )}
+            }
+          </Formik>
+        </Modal>
+      </div >
     )
   }
 }

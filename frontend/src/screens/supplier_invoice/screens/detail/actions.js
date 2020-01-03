e52099cf392a17176032b699d3e6eeb03f47c4ff
Jiten <udaykumar.mca@gmail.com>
2020-01-03 20:30:30 +0530

    Supplier Invoice CRUD COMPLETED

--- frontend/src/screens/supplier_invoice/screens/detail/actions.js
+++ frontend/src/screens/supplier_invoice/screens/detail/actions.js
@@ -1,11 +1,51 @@
 import { SUPPLIER_INVOICE } from 'constants/types'
 import {
   api,
-  authApi
+  authApi,
+  authFileUploadApi
 } from 'utils'
 
-export const initialData = (obj) => {
+export const getInvoiceById = (_id) => {
   return (dispatch) => {
-    
+    let data = {
+      method: 'GET',
+      url: `/rest/invoice/getInvoiceById?id=${_id}`
+    }
+
+    return authApi(data).then(res => {
+      return res
+    }).catch(err => {
+      throw err
+    })
+  }
+}
+
+export const updateInvoice = (obj) => {
+  return (dispatch) => {
+    let data = {
+      method: 'post',
+      url: '/rest/invoice/update',
+      data: obj
+    }
+    return authFileUploadApi(data).then(res => {
+      return res
+    }).catch(err => {
+      throw err
+    })
+  }
+}
+
+export const deleteInvoice = (id) => {
+  return (dispatch) => {
+    let data = {
+      method: 'DELETE',
+      url: `/rest/invoice/delete?id=${id}`
+    }
+
+    return authApi(data).then(res => {
+      return res
+    }).catch(err => {
+      throw err
+    })
   }
 }

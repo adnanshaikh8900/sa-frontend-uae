70cc17cce125e31742432cb3f4cf85e578d1fa33
Saurabh G <saurabh.gaikwad@daynilgroup.com>
2020-02-27 10:52:01 +0530

    added expense status for expense posting workflow

---
+++ src/main/java/com/simplevat/constant/ExpenseStatusEnum.java
@@ -0,0 +1,57 @@
+package com.simplevat.constant;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public enum ExpenseStatusEnum {
+
+	SAVED("Saved", 1), 
+	PENDING("Pending", 2), 
+	POST("Post", 3), 
+	APPROVED("Approved", 4),
+	PARTIALLY_PAID("Partially Paid", 5), 
+	PAID("Paid", 6);
+
+	@Getter
+	@Setter
+	private String desc;
+
+	@Getter
+	@Setter
+	private Integer value;
+
+	ExpenseStatusEnum(final String desc, Integer value) {
+		this.desc = desc;
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return this.desc;
+	}
+
+	public static List<ExpenseStatusEnum> getInvoiceStatusList() {
+		List<ExpenseStatusEnum> statusEnums = new ArrayList<ExpenseStatusEnum>();
+		for (ExpenseStatusEnum statusEnum : values()) {
+			statusEnums.add(statusEnum);
+		}
+		return statusEnums;
+	}
+
+	public static Map<Integer, ExpenseStatusEnum> map() {
+		Map<Integer, ExpenseStatusEnum> invoiceStatusMap = new HashMap<>();
+		for (ExpenseStatusEnum status : values()) {
+			invoiceStatusMap.put(status.getValue(), status);
+		}
+		return invoiceStatusMap;
+	}
+
+	public static String getExpenseStatusByValue(Integer value) {
+		return map().get(value).getDesc();
+	}
+}

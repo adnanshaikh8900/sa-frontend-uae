f8928822c4de88cd2a686f9ac4368ee33187a715
Saurabh G <saurabh.gaikwad@daynilgroup.com>
2020-03-04 18:30:00 +0530

    added pagination for currency

--- src/main/java/com/simplevat/rest/datalistcontroller/DataListController.java
+++ src/main/java/com/simplevat/rest/datalistcontroller/DataListController.java
@@ -12,8 +12,13 @@ import com.simplevat.entity.bankaccount.BankAccountType;
 import com.simplevat.entity.bankaccount.ChartOfAccount;
 import com.simplevat.constant.ContactTypeEnum;
 import com.simplevat.constant.InvoiceStatusEnum;
+import com.simplevat.constant.dbfilter.CurrencyFilterEnum;
+import com.simplevat.constant.dbfilter.ExpenseFIlterEnum;
+import com.simplevat.constant.dbfilter.ORDERBYENUM;
 import com.simplevat.rest.DropdownModel;
 import com.simplevat.rest.EnumDropdownModel;
+import com.simplevat.rest.PaginationModel;
+import com.simplevat.rest.PaginationResponseModel;
 import com.simplevat.service.BankAccountTypeService;
 import com.simplevat.service.CountryService;
 import com.simplevat.service.CurrencyService;
@@ -23,7 +28,10 @@ import io.swagger.annotations.ApiOperation;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -39,125 +47,130 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/rest/datalist")
 public class DataListController implements Serializable {
 
-    @Autowired
-    private CountryService countryService;
+	@Autowired
+	private CountryService countryService;
 
-    @Autowired
-    private CurrencyService currencyService;
+	@Autowired
+	private CurrencyService currencyService;
 
-    @Autowired
-    private BankAccountTypeService bankAccountTypeService;
+	@Autowired
+	private BankAccountTypeService bankAccountTypeService;
 
-    @Autowired
-    private ChartOfAccountService transactionTypeService;
+	@Autowired
+	private ChartOfAccountService transactionTypeService;
 
-    @Autowired
-    private IndustryTypeService industryTypeService;
+	@Autowired
+	private IndustryTypeService industryTypeService;
 
-    @GetMapping(value = "/getcountry")
-    public ResponseEntity getCountry() {
-        try {
-            List<Country> countryList = countryService.getCountries();
-            if (countryList != null && !countryList.isEmpty()) {
-                return new ResponseEntity<>(countryList, HttpStatus.OK);
-            } else {
-                return new ResponseEntity(HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+	@GetMapping(value = "/getcountry")
+	public ResponseEntity getCountry() {
+		try {
 
-    @Deprecated
-    @GetMapping(value = "/getcurrenncy")
-    public ResponseEntity getCurrency() {
-        try {
-            List<Currency> currencies = currencyService.getCurrencies();
-            if (currencies != null && !currencies.isEmpty()) {
-                return new ResponseEntity<>(currencies, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+			List<Country> countryList = countryService.getCountries();
+			if (countryList != null && !countryList.isEmpty()) {
+				return new ResponseEntity<>(countryList, HttpStatus.OK);
+			} else {
+				return new ResponseEntity(HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 
-    @ApiOperation(value = "All Transaction Types")
-    @GetMapping(value = "/getTransactionTypes")
-    public ResponseEntity getTransactionTypes() {
-        try {
-            List<ChartOfAccount> transactionTypes = transactionTypeService.findAll();
-            if (transactionTypes != null && !transactionTypes.isEmpty()) {
-                return new ResponseEntity<>(transactionTypes, HttpStatus.OK);
-            } else {
-                return new ResponseEntity(HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+	@Deprecated
+	@GetMapping(value = "/getcurrenncy")
+	public ResponseEntity getCurrency(PaginationModel paginationModel) {
+		try {
+			Map<CurrencyFilterEnum, Object> filterDataMap = new HashMap<CurrencyFilterEnum, Object>();
+			filterDataMap.put(CurrencyFilterEnum.ORDER_BY, ORDERBYENUM.DESC);
+			filterDataMap.put(CurrencyFilterEnum.DELETE_FLAG, false);
 
-    @ApiOperation(value = "All Invoice Status Types")
-    @GetMapping(value = "/getInvoiceStatusTypes")
-    public ResponseEntity getInvoiceStatusTypes() {
-        try {
-            List<InvoiceStatusEnum> statusEnums = InvoiceStatusEnum.getInvoiceStatusList();
-            List<DropdownModel> dropdownModels = new ArrayList<>();
-            if (statusEnums != null && !statusEnums.isEmpty()) {
-                for (InvoiceStatusEnum statusEnum : statusEnums) {
-                    dropdownModels.add(new DropdownModel(statusEnum.getValue(), statusEnum.getDesc()));
-                }
-                return new ResponseEntity<>(dropdownModels, HttpStatus.OK);
-            } else {
-                return new ResponseEntity(HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+			PaginationResponseModel response = currencyService.getCurrencies(filterDataMap, paginationModel);
+			if (response != null) {
+				return new ResponseEntity<>(response, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 
-    @ApiOperation(value = "All Contact Types")
-    @GetMapping(value = "/getContactTypes")
-    public ResponseEntity getContactTypes() {
-        try {
-            List<ContactTypeEnum> typeEnums = Arrays.asList(ContactTypeEnum.values());
-            List<DropdownModel> dropdownModels = new ArrayList<>();
-            if (typeEnums != null && !typeEnums.isEmpty()) {
-                for (ContactTypeEnum typeEnum : typeEnums) {
-                    dropdownModels.add(new DropdownModel(typeEnum.getValue(), typeEnum.getDesc()));
-                }
-                return new ResponseEntity<>(dropdownModels, HttpStatus.OK);
-            } else {
-                return new ResponseEntity(HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+	@ApiOperation(value = "All Transaction Types")
+	@GetMapping(value = "/getTransactionTypes")
+	public ResponseEntity getTransactionTypes() {
+		try {
+			List<ChartOfAccount> transactionTypes = transactionTypeService.findAll();
+			if (transactionTypes != null && !transactionTypes.isEmpty()) {
+				return new ResponseEntity<>(transactionTypes, HttpStatus.OK);
+			} else {
+				return new ResponseEntity(HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 
-    @ApiOperation(value = "All Industry Types")
-    @GetMapping(value = "/getIndustryTypes")
-    public ResponseEntity getIndustryTypes() {
-        try {
-            List<DropdownModel> dropdownModels = new ArrayList<>();
-            List<IndustryType> industryTypes = industryTypeService.getIndustryTypes();
-            if (industryTypes != null && !industryTypes.isEmpty()) {
-                for (IndustryType type : industryTypes) {
-                    dropdownModels.add(new DropdownModel(type.getId(), type.getIndustryTypeName()));
-                }
-                return new ResponseEntity<>(dropdownModels, HttpStatus.OK);
-            } else {
-                return new ResponseEntity(HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+	@ApiOperation(value = "All Invoice Status Types")
+	@GetMapping(value = "/getInvoiceStatusTypes")
+	public ResponseEntity getInvoiceStatusTypes() {
+		try {
+			List<InvoiceStatusEnum> statusEnums = InvoiceStatusEnum.getInvoiceStatusList();
+			List<DropdownModel> dropdownModels = new ArrayList<>();
+			if (statusEnums != null && !statusEnums.isEmpty()) {
+				for (InvoiceStatusEnum statusEnum : statusEnums) {
+					dropdownModels.add(new DropdownModel(statusEnum.getValue(), statusEnum.getDesc()));
+				}
+				return new ResponseEntity<>(dropdownModels, HttpStatus.OK);
+			} else {
+				return new ResponseEntity(HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ApiOperation(value = "All Contact Types")
+	@GetMapping(value = "/getContactTypes")
+	public ResponseEntity getContactTypes() {
+		try {
+			List<ContactTypeEnum> typeEnums = Arrays.asList(ContactTypeEnum.values());
+			List<DropdownModel> dropdownModels = new ArrayList<>();
+			if (typeEnums != null && !typeEnums.isEmpty()) {
+				for (ContactTypeEnum typeEnum : typeEnums) {
+					dropdownModels.add(new DropdownModel(typeEnum.getValue(), typeEnum.getDesc()));
+				}
+				return new ResponseEntity<>(dropdownModels, HttpStatus.OK);
+			} else {
+				return new ResponseEntity(HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ApiOperation(value = "All Industry Types")
+	@GetMapping(value = "/getIndustryTypes")
+	public ResponseEntity getIndustryTypes() {
+		try {
+			List<DropdownModel> dropdownModels = new ArrayList<>();
+			List<IndustryType> industryTypes = industryTypeService.getIndustryTypes();
+			if (industryTypes != null && !industryTypes.isEmpty()) {
+				for (IndustryType type : industryTypes) {
+					dropdownModels.add(new DropdownModel(type.getId(), type.getIndustryTypeName()));
+				}
+				return new ResponseEntity<>(dropdownModels, HttpStatus.OK);
+			} else {
+				return new ResponseEntity(HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 }
--- src/main/java/com/simplevat/service/CurrencyService.java
+++ src/main/java/com/simplevat/service/CurrencyService.java
@@ -1,28 +1,35 @@
 package com.simplevat.service;
 
+import com.simplevat.constant.dbfilter.CurrencyFilterEnum;
 import com.simplevat.entity.Currency;
 import com.simplevat.entity.CurrencyConversion;
+import com.simplevat.rest.PaginationModel;
+import com.simplevat.rest.PaginationResponseModel;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by mohsin on 3/11/2017.
  */
 public abstract class CurrencyService extends SimpleVatService<Integer, Currency> {
 
-    public abstract List<Currency> getCurrencies();
+	public abstract List<Currency> getCurrencies();
 
-    public abstract Currency getCurrency(final int currencyCode);
+	public abstract Currency getCurrency(final int currencyCode);
 
-    public abstract Currency getDefaultCurrency();
+	public abstract Currency getDefaultCurrency();
 
-    public abstract CurrencyConversion getCurrencyRateFromCurrencyConversion(final int currencyCode);
+	public abstract CurrencyConversion getCurrencyRateFromCurrencyConversion(final int currencyCode);
 
-    public abstract String getCountryCodeAsString(String CountryCode);
+	public abstract String getCountryCodeAsString(String CountryCode);
 
-    public abstract List<Currency> getCurrencyList(Currency currency);
+	public abstract List<Currency> getCurrencyList(Currency currency);
 
-    public abstract List<String> getCountryCodeString();
+	public abstract List<String> getCountryCodeString();
 
-    public abstract Boolean isCurrencyDataAvailableOnTodayDate();
+	public abstract Boolean isCurrencyDataAvailableOnTodayDate();
+
+	public abstract PaginationResponseModel getCurrencies(Map<CurrencyFilterEnum, Object> filterDataMap,
+			PaginationModel paginationModel);
 }

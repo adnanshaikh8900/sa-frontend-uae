c71837f3dbf101b22532596eab404b9c48bb4666
Jiten <jiten.gudhaka@daynilgroup.com>
2020-04-06 11:20:23 +0530

    Code Updated for Invoice

--- frontend/src/screens/supplier_invoice/screen.js
+++ frontend/src/screens/supplier_invoice/screen.js
@@ -228,7 +228,7 @@ class SupplierInvoice extends React.Component {
             <DropdownItem onClick={() => this.props.history.push('/admin/expense/supplier-invoice/view', { id: row.id })}>
               <i className="fas fa-eye" /> View
             </DropdownItem>
-            <DropdownItem onClick={() => { this.sendEmail(row.id)}}>
+            <DropdownItem onClick={() => { this.sendMail(row.id) }}>
               <i className="fas fa-upload" /> Send
             </DropdownItem>
             {/* <DropdownItem>
@@ -246,22 +246,15 @@ class SupplierInvoice extends React.Component {
     )
   }
 
-  sendEmail = (id) => {
-    const postData = {
-      id
-    }
-    this.props.supplierInvoiceActions.checkMailConfiguration().then(res => {
+  sendMail = (id) => {
+    this.props.supplierInvoiceActions.sendMail(id).then(res => {
       if (res.status === 200) {
-          this.props.supplierInvoiceActions.sendEmail(postData).then(res => {
-            if(res.status === 200)  {
-               this.props.commonActions.tostifyAlert('success', 'Invoice Send Successfully');
-            }
-          })
+        this.props.commonActions.tostifyAlert('success', 'Invoice Send Successfully');
       }
     }).catch(err => {
-        this.props.commonActions.tostifyAlert('error', 'Please First fill The Mail Configuration Detail');
-  })
-}
+      this.props.commonActions.tostifyAlert('error', 'Please First fill The Mail Configuration Detail');
+    })
+  }
 
   onSizePerPageList = (sizePerPage) => {
     if (this.options.sizePerPage !== sizePerPage) {
@@ -388,7 +381,7 @@ class SupplierInvoice extends React.Component {
     })
   }
 
-  openInvoicePreviewModal = (id) =>{
+  openInvoicePreviewModal = (id) => {
     this.setState({
       selectedId: id
     }, () => {

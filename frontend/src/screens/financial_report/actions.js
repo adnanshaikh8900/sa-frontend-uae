df6fca8eaf3c545557c079a9b327b93b17df634e
Afzal <afzalrakhan@gmail.com>
2020-04-27 15:06:38 +0530

    added and updated files for profit and loss financial report.

--- frontend/src/screens/financial_report/actions.js
+++ frontend/src/screens/financial_report/actions.js
@@ -1,9 +1,22 @@
 import {
-  // authApi
+  authApi
 } from 'utils'
 
-export const initialData = (obj) => {
+export const getProfitAndLossReport = (postData) => {
+  const { startDate, endDate} = postData
+  let url = `rest/financialReport/profitandloss?startDate=${startDate}&endDate=${endDate}`
+
   return (dispatch) => {
-    
+    let data = {
+      method: 'get',
+      url
+    }
+    return authApi(data).then((res) => {
+      if (res.status === 200) {
+        return res
+      }
+    }).catch((err) => {
+      throw err
+    })
   }
 }
--- src/main/java/com/simplevat/service/JournalLineItemService.java
+++ src/main/java/com/simplevat/service/JournalLineItemService.java
@@ -1,13 +1,18 @@
 package com.simplevat.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.simplevat.entity.JournalLineItem;
 import com.simplevat.rest.detailedgeneralledgerreport.ReportRequestModel;
+import com.simplevat.rest.financialreport.CreditDebitAggregator;
+import com.simplevat.rest.financialreport.FinancialReportRequestModel;
 
 public abstract class JournalLineItemService extends SimpleVatService<Integer, JournalLineItem> {
 
     public abstract void deleteByJournalId(Integer journalId);
 
     public abstract List<JournalLineItem> getList(ReportRequestModel reportRequestModel);
+
+    public abstract Map<Integer, CreditDebitAggregator> getAggregateTransactionCategoryMap(FinancialReportRequestModel reportRequestModel);
 }

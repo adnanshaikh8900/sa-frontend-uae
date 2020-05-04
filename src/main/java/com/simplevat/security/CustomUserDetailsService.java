576fe31ec06259302ef7c08d7887da1ed6699498
Saurabh G <saurabh.gaikwad@daynilgroup.com>
2020-05-04 18:32:51 +0530

    solved coa  drop down issue

--- src/main/java/com/simplevat/security/CustomUserDetailsService.java
+++ src/main/java/com/simplevat/security/CustomUserDetailsService.java
@@ -1,6 +1,5 @@
 package com.simplevat.security;
 
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 

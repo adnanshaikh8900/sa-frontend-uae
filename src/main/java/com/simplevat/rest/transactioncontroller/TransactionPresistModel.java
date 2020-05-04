8e37d2d6993ca247aee31ab0ee11e687a9e1cc5c
Saurabh G <saurabh.gaikwad@daynilgroup.com>
2020-05-04 23:15:32 +0530

    updated api for transaction save update

--- src/main/java/com/simplevat/rest/transactioncontroller/TransactionPresistModel.java
+++ src/main/java/com/simplevat/rest/transactioncontroller/TransactionPresistModel.java
@@ -7,12 +7,12 @@ package com.simplevat.rest.transactioncontroller;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.Date;
-
-import javax.validation.constraints.NotNull;
+import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
+
+import com.simplevat.constant.TransactionExplinationStatusEnum;
+import com.simplevat.rest.ReconsileRequestLineItemModel;
 
 import lombok.Data;
 
@@ -23,20 +23,51 @@ import lombok.Data;
 @Data
 public class TransactionPresistModel implements Serializable {
 
-	private Integer id;
-    private Date transactionDate;
-    private String transactionDescription;
-    private BigDecimal transactionAmount;
-    @NotNull
-    private Integer chartOfAccountId;
-    private Integer transactionCategoryId;
-    @NotNull
-    private Integer bankAccountId;
-    private Integer projectId;
-    private String receiptNumber;
-    private String attachementDescription;
-    private MultipartFile attachment;
-    private String receiptAttachmentPath;
-    private String receiptAttachmentFileName;
+	// TODO : REMOVE AFTER TESTING 4-5-2020
+
+//	private Integer id;
+//    private Date transactionDate;
+//    private String transactionDescription;
+//    private BigDecimal transactionAmount;
+//    @NotNull
+//    private Integer chartOfAccountId;
+//    private Integer transactionCategoryId;
+//    @NotNull
+//    private Integer bankAccountId;
+//    private Integer projectId;
+//    private String receiptNumber;
+//    private String attachementDescription;
+//    private MultipartFile attachment;
+//    private String receiptAttachmentPath;
+//    private String receiptAttachmentFileName;
+
+	private String DATE_FORMAT = "dd/MM/yyyy";
+
+	private Integer bankId;
+	private Integer transactionId;
+	private Integer coaCategoryId;;
+	private Integer transactionCategoryId;
+	private BigDecimal amount;
+	private String date;
+	private String description;
+	private MultipartFile attachmentFile;
+	private String reference;
+
+	// EXPENSE
+	private Integer vatId;
+	private Integer vendorId;
+	private Integer customerId;
+
+	// MONEY PAID TO USER
+	// MONEY RECEIVED FROM OTHER
+	private Integer employeeId;
+
+	// Transafer To
+	private Integer reconsileBankId;
+
+	// SALES
+	private List<ReconsileRequestLineItemModel> invoiceIdList;
+
+	TransactionExplinationStatusEnum explinationStatusEnum;
 
 }

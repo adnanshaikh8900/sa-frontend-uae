74da3825632a5f714b1308d7e2c08587d09e45c1
navins94 <nikshingote@gmail.com>
2020-06-08 18:16:19 +0530

    record payment date fix

--- frontend/src/screens/bank_account/screens/transactions/sections/explain_transaction_detail.js
+++ frontend/src/screens/bank_account/screens/transactions/sections/explain_transaction_detail.js
@@ -85,7 +85,7 @@ class ExplainTrasactionDetail extends React.Component {
 		this.props.transactionDetailActions
 			.getTransactionDetail(selectedData.id)
 			.then((res) => {
-				this.getChartOfAccountCategoryList(selectedData.debitCreditFlag);
+				this.getChartOfAccountCategoryList('D');
 				this.setState(
 					{
 						loading: false,

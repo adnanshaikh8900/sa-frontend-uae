11adc1005b8c94d648561e09d37024aefe82717a
Adil Khan <Adil.Khan@gulftranstech.com>
2020-06-18 16:55:22 +0530

    Proper tax entry in detailed general ledger

--- frontend/src/constants/config.js
+++ frontend/src/constants/config.js
@@ -1,5 +1,5 @@
 export default {
 	API_ROOT_URL: 'http://k8s.dev.simplevat.com/',
-	//API_ROOT_URL: 'http://localhost:8083',
+	//API_ROOT_URL: 'http://localhost:8080',
 	// API_ROOT_URL: 'http://192.168.1.7:70'
 };

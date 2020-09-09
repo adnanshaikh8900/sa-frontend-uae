af27596a0dfd154eb16b64536da77fe913434579
Mohsin Hashmi <mohsin.hashmi@eradahcapital.com>
2020-09-09 21:40:17 +0400

    updated webhook

--- gcb_test/gcb_test.sh
+++ gcb_test/gcb_test.sh
@@ -5,5 +5,5 @@ curl \
   -T request.json \
   https://cloudbuild.googleapis.com/v1/projects/gtt-project-prod/builds
 
-# Test hello world to Teams webhook
-#curl -H 'Content-Type: application/json' -d '{\"text\": \"Hello World\"}' https://outlook.office.com/webhook/16dc3053-ea38-41ed-99ea-59b8fae52c37@290a4fa3-3f81-4c6e-960a-f51dda982495/IncomingWebhook/bbf0dfaf2c0246f48985badfa4efe9b0/8bafecc0-777b-458e-91f7-4452bd516c11
+#Test hello world to Teams webhook
+#curl -H 'Content-Type: application/json' -d '{\"text\": \"Hello World\"}' https://outlook.office.com/webhook/619cdcba-0909-451b-b094-9229da967e58@dc296c81-6e73-46a8-b2b9-f7854f41f3fd/IncomingWebhook/9735385ac3634cb49fc497d858ccfb41/56bd4d9c-54ec-4c85-9c4e-0e2bcad0c95d

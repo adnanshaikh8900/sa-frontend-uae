135a629ec244b5daf9e93dd44bacdb98d6f70bb2
navins94 <nikshingote@gmail.com>
2020-09-12 00:45:09 +0530

    config url

--- frontend/src/constants/config.js
+++ frontend/src/constants/config.js
@@ -1,5 +1,5 @@
 export default {
 	//API_ROOT_URL: 'http://k8s.dev.simplevat.com/',
-	API_ROOT_URL: 'http://localhost:8082',
-	//API_ROOT_URL: process.env.SIMPLEVAT_HOST,
+	//API_ROOT_URL: 'http://localhost:8080',
+	API_ROOT_URL: process.env.SIMPLEVAT_HOST,
 };

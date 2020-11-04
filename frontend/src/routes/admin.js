cbab6647e0410505c806b4d2d92272605261d71c
navins94 <nikshingote@gmail.com>
2020-11-04 20:21:48 +0530

    fix: general settings route

--- frontend/src/routes/admin.js
+++ frontend/src/routes/admin.js
@@ -494,7 +494,7 @@ const adminRoutes = [
 
 	{
 		path: '/admin/settings/general',
-		name: 'General Settings',
+		name: 'SaveGeneralSetting',
 		component: GeneralSettings.screen,
 	},
 	// {
@@ -556,7 +556,7 @@ const adminRoutes = [
 
 	{
 		path: '/admin/profile',
-		name: 'Profile',
+		name: 'UpdateProfile',
 		component: Profile.screen,
 	},
 

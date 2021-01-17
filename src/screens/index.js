326dc40f38b68853a4e4651a844c72b55b2db7c4
muzammil6202 <muzammil6202>
2021-01-17 12:18:58 +0530

    commit

--- src/screens/index.js
+++ src/screens/index.js
@@ -11,8 +11,8 @@ import Journal from './journal';
 import CreateJournal from './journal/screens/create';
 import DetailJournal from './journal/screens/detail';
 import OpeningBalance from './opening_balance';
-// import CreateOpeningBalance from './opening_balance/screens/create'
-// import DetailOpeningBalance from './opening_balance/screens/detail'
+import CreateOpeningBalance from './opening_balance/screens/create'
+import DetailOpeningBalance from './opening_balance/screens/detail'
 
 import BankAccount from './bank_account';
 import CreateBankAccount from './bank_account/screens/create';
@@ -115,8 +115,8 @@ export {
 	CreateJournal,
 	DetailJournal,
 	OpeningBalance,
-	// CreateOpeningBalance,
-	// DetailOpeningBalance,
+	CreateOpeningBalance,
+	DetailOpeningBalance,
 	BankAccount,
 	CreateBankAccount,
 	DetailBankAccount,

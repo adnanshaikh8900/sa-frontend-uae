25776dece43af0139ad4788c3c132010435dc2ac
muzammil6202 <muzammil6202>
2021-01-22 00:14:44 +0530

    Bug 211: We are showing the wrong message for a single second on opening the balance tab on every refresh.

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function Loading () {
   return (
-    <div className="animated fadeIn pt-3 text-center">
-      <div className="sk-spinner sk-spinner-pulse"></div>
-    </div>
+    <div className="sk-double-bounce loader">
+    <i class="fa fa-refresh fa-spin fa-3x fa-fw"></i>
+    <span class="sr-only">Loading...</span>
+  </div>
   )
 }

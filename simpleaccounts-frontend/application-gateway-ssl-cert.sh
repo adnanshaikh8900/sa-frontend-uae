9a84c5c0c036d31d798cff30fa029b27614513c1
mohsin <l5nr7fl2lfsaaepdp44spkuhj2huzuksja3lfsuerolajq56sb6a>
2021-02-18 10:54:29 +0400

    fixed invalid certificate issue

--- simpleaccounts-frontend/application-gateway-ssl-cert.sh
+++ simpleaccounts-frontend/application-gateway-ssl-cert.sh
@@ -14,6 +14,7 @@ helm install $subdomain-frontend ./$helmDir --values ./$helmDir/values.yaml \
 --set ingress.hosts[0].paths[0]="/*" \
 --set ingress.tls[0].hosts[0]=$subdomain-api.$maindomain \
 --set ingress.tls[0].hosts[1]=$subdomain.$maindomain \
+--set ingress.tls[0].secretName=wildcard-app-simpleaccounts-io-tls \
 -n $nameserver \
 --dry-run --debug
 
@@ -30,33 +31,10 @@ helm upgrade $subdomain-frontend ./$helmDir --values ./$helmDir/values.yaml \
 --set ingress.hosts[0].paths[0]="/*" \
 --set ingress.tls[0].hosts[0]=$subdomain-api.$maindomain \
 --set ingress.tls[0].hosts[1]=$subdomain.$maindomain \
+--set ingress.tls[0].secretName=wildcard-app-simpleaccounts-io-tls \
 -n $nameserver \
 --dry-run --debug
 
 helm uninstall $subdomain-frontend -n $nameserver
 
 helm rollback $subdomain-frontend 0  -n $nameserver
-
-
-*************
-
-nameserver="simpleaccounts-app"
-maindomain="app.simpleaccounts.io"
-subdomain="test1-app"
-helmDir="simpleaccounts-frontend"
-SVrelease="latest"
-
-helm install $subdomain-frontend ./$helmDir --values ./$helmDir/values.yaml \
---set simpleVatBackendRelease=$SVrelease \
---set image.repository.frontend.tag=$SVrelease \
---set simpleVatHost=https://$subdomain-api.$maindomain \
---set fullnameOverride=$subdomain-frontend \
---set serviceAccount.name=$subdomain-deploy-robot-frontend \
---set ingress.hosts[0].host=$subdomain.$maindomain \
---set ingress.hosts[0].paths[0]="/*" \
---set ingress.tls[0].hosts={$subdomain.$maindomain} \
---set image.repository.frontend.imageName=prakhar1989/static-site \
--n $nameserver \
---dry-run --debug
-
-***************

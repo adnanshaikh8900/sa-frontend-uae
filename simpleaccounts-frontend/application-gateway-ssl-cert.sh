b4fe189f2a1107435317c78029389a7002ea52a7
mohsin <l5nr7fl2lfsaaepdp44spkuhj2huzuksja3lfsuerolajq56sb6a>
2021-02-18 12:54:01 +0400

    helm deployment issue

--- simpleaccounts-frontend/application-gateway-ssl-cert.sh
+++ simpleaccounts-frontend/application-gateway-ssl-cert.sh
@@ -2,7 +2,7 @@ nameserver="simpleaccounts-app"
 maindomain="app.simpleaccounts.io"
 subdomain="load-me"
 helmDir="simpleaccounts-frontend"
-SVrelease="0.0.3-alpha-394"
+SVrelease="0.0.3-alpha-403"
 
 helm install $subdomain-frontend ./$helmDir --values ./$helmDir/values.yaml \
 --set simpleVatBackendRelease=$SVrelease \

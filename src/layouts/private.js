f23924f73c2ceb4584ca28cb6006501ad5a48ac1
ironpro <darlingboxing@gmail.com>
2021-02-27 15:08:01 +0100

    task 211-showing wrong message issue

--- src/layouts/private.js
+++ src/layouts/private.js
@@ -2,25 +2,32 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 const permission = require('assets/images/settings/permission-removebg.png');
 const PrivateRoute = ({ component: Component, name, node, ...rest }) => {
-	let found = node.some((ele) => ele.moduleName === name);
-	return (
-		<Route
-			{...rest}
-			render={(props) =>
-				node && found ? (
-					<Component {...props} />
-				) : (
-					<center>
-					<div ><i class="fas fa-exclamation-triangle fa-8x" >
-					</i>
-					<br></br><br></br>
-						<b>You Are Not Allowed to view this page</b>
-					</div>
-					</center>
-				)
-			}
-		/>
-	);
+	if(node.length !== 0) {
+		let found = node.some((ele) => ele.moduleName === name);
+		return (
+			<Route
+				{...rest}
+				render={(props) =>
+					node && found ? (
+						<Component {...props} />
+					) : (
+						<center>
+						<div ><i class="fas fa-exclamation-triangle fa-8x" >
+						</i>
+						<br></br><br></br>
+							<b>You Are Not Allowed to view this page</b>
+						</div>
+						</center>
+					)
+				}
+			/>
+		);
+	}
+	else {
+		return (
+			<div></div>
+		);
+	}
 };
 
 export default PrivateRoute;

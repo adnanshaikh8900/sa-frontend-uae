0269caed13db2ac3d6844a70b6670be7fae7edf6
Suraj Rahade <Suraj.Rahade@datainn.io>
2021-05-28 14:01:04 +0530

    commit for payroll getsalarySlip Api integration

--- src/screens/payrollemp/screens/view/sections/viewPayslip.js
+++ src/screens/payrollemp/screens/view/sections/viewPayslip.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import logo from 'assets/images/brand/logo.png';
 import {
 	Button,
 	Row,
@@ -15,7 +15,10 @@ import {
 	ModalFooter,
 	UncontrolledTooltip,
 	CardBody,
+	Table,
+	Card,
 } from 'reactstrap';
+import { toInteger, upperCase, upperFirst } from 'lodash';
 import { Formik, Field } from 'formik';
 import Select from 'react-select';
 import * as Yup from 'yup';
@@ -35,6 +38,8 @@ import LocalizedStrings from 'react-localization';
 import '../style.scss';
 import { PDFExport } from '@progress/kendo-react-pdf';
 import ReactToPrint from 'react-to-print';
+
+var converter = require('number-to-words');
 
 const mapStateToProps = (state) => {
 
@@ -79,17 +84,20 @@ class PaySlipModal extends React.Component {
 
 
 
-    static getDerivedStateFromProps(nextProps, prevState) {
-        if (prevState.selectedData !== nextProps.selectedData || prevState.totalAmount !== nextProps.totalAmount ||
-			prevState.totalVatAmount != nextProps.totalVatAmount  ) {
-			console.log('getDerivedStateFromProps state changed',nextProps.selectedData.poQuatationLineItemRequestModelList);
+	static getDerivedStateFromProps(nextProps, prevState) {
+		if (prevState.selectedData !== nextProps.selectedData) {
+			console.log('getDerivedStateFromProps state changed', nextProps.selectedData);
+			debugger
+			return {
+				prefixData: nextProps.prefixData,
+				selectedData: nextProps.selectedData,
+				Fixed:nextProps.Fixed,
+				FixedAllowance:nextProps.FixedAllowance,
+				Deduction:nextProps.Deduction,
+				Variable:nextProps.Variable,
 
-		 return { prefixData : nextProps.prefixData,
-		 	selectedData :nextProps.selectedData,
-			 totalAmount :nextProps.totalAmount,
-			 totalVatAmount :nextProps.totalVatAmount,
-		};
-        }
+			};
+		}
 		// else if(prevState.totalAmount !== nextProps.totalAmount)
 		// {
 		// 	console.log('+++++++++++++++++',nextProps.totalAmount)
@@ -104,7 +112,7 @@ class PaySlipModal extends React.Component {
 		// 		totalVatAmount :nextProps.totalVatAmount
 		// 	};
 		// }
-    }
+	}
 
 
 	// Create
@@ -126,11 +134,11 @@ class PaySlipModal extends React.Component {
 
 	render() {
 		strings.setLanguage(this.state.language);
-		const { openModal, closeModal, id } = this.props;
+		const { openModal, closeModal, id, companyData } = this.props;
 		const { initValue, contentState, data, supplierId } = this.state;
 
 		let tmpSupplier_list = []
-
+console.log(this.state.Variable,"Variable")
 
 		return (
 			<div className="contact-modal-screen">
@@ -140,18 +148,8 @@ class PaySlipModal extends React.Component {
 							<div className="animated fadeIn">
 								<Row>
 									<Col lg={12} className="mx-auto">
-										<div className="pull-right" style={{display:"inline-flex"}}>
-											{/* <Button
-									className="btn btn-sm edit-btn"
-									onClick={() => {
-										this.props.history.push(
-											'/admin/revenue/customer-invoice/detail',
-											{ id },
-										);
-									}}
-								>
-									<i className="fa fa-pencil"></i>
-								</Button> */}
+										<div className="pull-right" style={{ display: "inline-flex" }}>
+
 											<Button
 												className="mr-2 print-btn-cont"
 												onClick={() => {
@@ -168,15 +166,15 @@ class PaySlipModal extends React.Component {
 												)}
 												content={() => this.componentRef}
 											/> */}
-												<div
-													className="mr-2 print-btn-cont"
-													onClick={() => window.print()}
-													style={{
-														cursor: 'pointer',
-														}}
-												>
-													<i className="fa fa-print"></i>
-												</div>
+											<div
+												className="mr-2 print-btn-cont"
+												onClick={() => window.print()}
+												style={{
+													cursor: 'pointer',
+												}}
+											>
+												<i className="fa fa-print"></i>
+											</div>
 											<Button
 												type="button"
 												className=" print-btn-cont"
@@ -193,16 +191,242 @@ class PaySlipModal extends React.Component {
 												ref={(component) => (this.pdfExportComponent = component)}
 												scale={0.8}
 												paperSize="A4"
-												
+
 											>
-												<CardBody id="section-to-print"><div> helooww </div></CardBody>	
-												{/* <InvoiceTemplate
-										invoiceData={invoiceData}
-										currencyData={currencyData}
-										ref={(el) => (this.componentRef = el)}
-										totalNet={this.state.totalNet}
-										companyData={profile}
-									/> */}
+											
+												<CardBody  id="section-to-print" style={{}}>
+													<div
+														style={{
+															width: '100%',
+															display: 'flex',
+															border: '1px solid',
+															padding: '7px', borderColor: '#c8ced3'
+														}}
+													>
+														<div style={{ width: '70%' }}>
+															<div className="companyDetails">
+																<img
+																	src={
+																		companyData &&
+																			companyData.company &&
+																			companyData.company.companyLogo
+																			? 'data:image/jpg;base64,' +
+																			companyData.company.companyLogo
+																			: logo
+																	}
+																	className=""
+																	alt=""
+																	style={{ width: ' 100px' }}
+																/>
+															</div>
+														</div>
+														<div style={{ width: '130%', justifyContent: 'center' }}>
+
+															<div
+																style={{
+																	width: '130%',
+																	fontSize: '1rem',
+																	fontWeight: '700',
+																	textTransform: 'uppercase',
+																	color: 'black',
+																}}
+															>
+																Payslip for the month of {this.state.selectedData.salaryMonth}
+															</div>
+
+														</div>
+														<div
+															style={{
+																width: '70%',
+																display: 'flex',
+																flexDirection: 'column',
+																justifyContent: 'right',
+															}}
+														>
+
+														</div>
+													</div>
+
+
+													<div
+														style={{
+															width: '100%',
+															display: 'flex',
+															justifyContent: 'space-between',
+															marginBottom: '1rem',
+															borderLeft: '1px solid',
+															borderRight: '1px solid',
+															borderBottom: '1px solid', borderColor: '#c8ced3'
+														}}
+													>
+														<div style={{ width: '185%' }}>
+															<div
+																style={{
+																	width: '95%',
+																	margin: '0.5rem',
+																	border: '1px solid', borderColor: '#c8ced3'
+																}}
+															>
+																<div className="mb-1 mt-1 ml-2">
+																	<b>Employee Pay Summary,</b>
+																</div>
+																<div className="m-2" style={{ fontSize: '10px' }}>
+																	<Row> <Col className='mt-2 mb-2' style={{ fontWeight: "630" }}>Employee Name </Col>
+																		<Col className='mt-2 mb-2'>: &nbsp;{this.state.selectedData.employeename !== '' ? this.state.selectedData.employeename : ('-')}</Col></Row>
+
+																	<Row> <Col className='mt-2 mb-2' style={{ fontWeight: "630" }}>Designation </Col><Col className='mt-2 mb-2'>: &nbsp;{this.state.selectedData.designation !== '' ? this.state.selectedData.designation : ('-')}</Col></Row>
+
+																	{/* <Row> <Col className='mt-2 mb-2'>Personal Email  </Col><Col className='mt-2 mb-2'>: &nbsp;{this.state.EmployeeDetails.email ? this.state.EmployeeDetails.email : ('-')}</Col></Row>				 */}
+
+																	<Row> <Col className='mt-2 mb-2' style={{ fontWeight: "630" }}>Date of Joining</Col><Col className='mt-2 mb-2'>: &nbsp;{this.state.selectedData.dateOfJoining !== '' ? this.state.selectedData.dateOfJoining : ('-')}</Col></Row>
+
+																	<Row> <Col className='mt-2 mb-2' style={{ fontWeight: "630" }}>Pay Period </Col><Col className='mt-2 mb-2'>: &nbsp;{this.state.selectedData.salaryMonth !== '' ? this.state.selectedData.salaryMonth : ('-')}</Col></Row>
+																	<Row> <Col className='mt-2 mb-2' style={{ fontWeight: "630" }}>Pay Date </Col><Col className='mt-2 mb-2'>: &nbsp;{this.state.selectedData.payDate !== '' ? this.state.selectedData.payDate : ('-')}</Col></Row>
+																</div>
+
+
+																{/* <div className="mb-1 ml-2"><b>Name:</b> {invoiceData.name}</div>
+																<div className="mb-1 ml-2"><b>Company:</b> {invoiceData.organisationName}</div>
+																<div className="mb-1 ml-2"><b>Email:</b> {invoiceData.email}</div>
+																<div className="mb-3 ml-2"><b>Address:</b> {invoiceData.address}</div> */}
+															</div>
+														</div>
+														<div
+															style={{
+																width: '100%',
+																display: 'flex',
+																justifyContent: 'space-between',
+																padding:"22px"
+															}}
+														>
+															<Card className="" style={{ width: '80%',margin:"1.5rem" ,fontSize:"10px" }}>
+																<div className="text-center pt-4">Employee Net Pay</div>
+																<div className="mb-1 ml-2 text-center"><h1 style={{ color: "#2064d8" }}><b>{this.state.selectedData.netPay}</b></h1></div>
+																<div className="mb-1 ml-2 text-center">Paid Days : {this.state.selectedData.noOfDays} <b>|</b>  LOP Days : {this.state.selectedData.lopDays}</div>
+															</Card>
+														</div>
+													</div>
+													<div
+														style={{
+															width: '100%',
+															display: "flex",
+															border: 'solid 1px', borderColor: '#c8ced3',
+															fontSize: "12px",
+
+														}}
+													>
+
+														<div className="pr-2" style={{ width: "50%" }}>
+															<Table >
+																<thead className="table-header-bg">
+																	<tr>
+																		{/* <th className="center" style={{ padding: '0.5rem', width: "40px" }}>																			#									</th> */}
+																		{/* <th style={{ padding: '0.5rem' }}>Item</th> */}
+																		<th style={{ padding: '0.5rem' }}>Earnings</th>
+																		<th style={{ padding: '0.5rem' }}>Amount</th>
+
+																	</tr>
+																</thead>
+																<tbody className=" table-bordered table-hover">
+															{this.state.Fixed &&
+															this.state.Fixed &&
+															this.state.Fixed.map((item, index) => {
+																	return (
+																		<tr key={index}>
+																			{/* <td className="center">{index + 1}</td> */}
+																			<td>{item.componentName}</td>
+																			<td>{item.componentValue}</td>
+																			
+																		</tr>
+																	);
+																})}
+																{this.state.FixedAllowance &&
+															this.state.FixedAllowance &&
+															this.state.FixedAllowance.map((item, index) => {
+																	return (
+																		<tr key={index}>
+																			{/* <td className="center">{index + 1}</td> */}
+																			<td>{item.componentName}</td>
+																			<td>{item.componentValue}</td>
+																			
+																		</tr>
+																	);
+																})}
+																
+																{this.state.Variable &&
+															this.state.Variable &&
+															this.state.Variable.map((item, index) => {
+																	return (
+																		<tr key={index}>
+																			{/* <td className="center">{index + 1}</td> */}
+																			<td>{item.componentName}</td>
+																			<td>{item.componentValue}</td>
+																			
+																		</tr>
+																	);
+																})}
+														</tbody>
+														<tfoot  className=" table-bordered table-hover">
+															<tr>
+																{/* <td></td> */}
+																<td><b>Gross Earnings</b></td>
+																<td>{this.state.selectedData.earnings}</td>
+															</tr>
+														</tfoot>
+															</Table>
+														</div>
+														<div className="pl-2"  style={{ width: "50%" }}>
+															<Table  >
+																<thead className="table-header-bg">
+																	<tr>
+																		<th className="center" style={{ padding: '0.5rem', width: "40px" }}>
+																			#
+									</th>
+																		{/* <th style={{ padding: '0.5rem' }}>Item</th> */}
+																		<th style={{ padding: '0.5rem' }}>Deductions</th>
+																		<th style={{ padding: '0.5rem' }}>Amount</th>
+
+																	</tr>
+																</thead>
+														<tbody className=" table-bordered table-hover">
+															{this.state.Deduction &&
+															this.state.Deduction &&
+															this.state.Deduction.map((item, index) => {
+																	return (
+																		<tr key={index}>
+																			{/* <td className="center">{index + 1}</td> */}
+																			<td>{item.componentName}</td>
+																			<td>{item.componentValue}</td>
+																			
+																		</tr>
+																	);
+																})}
+														</tbody>
+														<tfoot  className=" table-bordered table-hover">
+															<tr>
+																{/* <td></td> */}
+																<td><b>Total Deductions</b></td>
+																<td>{this.state.selectedData.deductions}</td>
+															</tr>
+														</tfoot>
+															</Table>
+
+														</div>
+
+													</div>
+													<div
+														style={{
+															width: '100%',
+
+															border: 'solid 1px', borderColor: '#c8ced3',
+															fontSize: "12px",
+															backgroundColor: "#e4f8ff"
+														}}
+													>
+														<h5>	<span style={{ color: "blue" }}>|</span> Total Net Payable <b>{this.state.selectedData.netPay}</b>  &nbsp; ( {upperFirst(converter.toWords(toInteger(this.state.selectedData.netPay)))} )
+														</h5>
+													</div>
+												</CardBody>
 											</PDFExport>
 										</div>
 									</Col>

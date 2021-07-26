11686d3b3c00b2c500ec0b6b5102726c75e9827d
Suraj Rahade <Suraj.Rahade@datainn.io>
2021-07-26 13:17:09 +0530

    Mail Themes

--- src/screens/template/actions.js
+++ src/screens/template/actions.js
@@ -1,3 +1,18 @@
 import { TEMPLATE } from 'constants/types';
 import { authApi } from 'utils';
 
+export const updateMailTheme = (id) => {
+	return (dispatch) => {
+		let data = {
+			method: 'Post',
+			url: `/rest/templates/updateMailTemplateTheme?templateId=${id}`,
+		};
+		return authApi(data)
+			.then((res) => {
+				return res;
+			})
+			.catch((err) => {
+				throw err;
+			});
+	};
+};
--- src/screens/template/screen.js
+++ src/screens/template/screen.js
@@ -15,6 +15,7 @@ import {
 
 import { InvoiceTemplate } from 'components'
 import bauhaus from 'assets/images/invoice-template/bauhaus.jpg';
+import Basic from 'assets/images/invoice-template/Basic.jpg';
 import simple from 'assets/images/invoice-template/simple.jpg';
 import horizon from 'assets/images/invoice-template/horizon.jpg';
 import ejs from 'assets/images/invoice-template/ejs.jpg';
@@ -53,20 +54,20 @@ class Template extends React.Component {
 								<Col lg={12}>
 									<div className="h4 mb-0 d-flex align-items-center">
 										<i className="nav-icon fas fa-boxes" />
-										<span className="ml-2 " >Invoice Templates</span>
+										<span className="ml-2 " >Mail Templates</span>
 									</div>
 								</Col>
 							</Row>
 						</CardHeader>
 						<CardBody>
-							<InvoiceTemplate templateId="1" templateTitle="Bauhaus" templateImg={bauhaus}></InvoiceTemplate>
+							<InvoiceTemplate templateId="1" templateTitle="Basic" templateImg={Basic}></InvoiceTemplate>
 							<InvoiceTemplate templateId="2" templateTitle="Simple" templateImg={simple}></InvoiceTemplate>
-							<InvoiceTemplate templateId="3" templateTitle="Horizon" templateImg={horizon}></InvoiceTemplate>
+							{/* <InvoiceTemplate templateId="3" templateTitle="Horizon" templateImg={horizon}></InvoiceTemplate>
 							<InvoiceTemplate templateId="4" templateTitle="Elliot Jay stocks" templateImg={ejs}></InvoiceTemplate>
 							<InvoiceTemplate templateId="5" templateTitle="Union" templateImg={union}></InvoiceTemplate>
 							<InvoiceTemplate templateId="6" templateTitle="Lola" templateImg={lola}></InvoiceTemplate>
 							<InvoiceTemplate templateId="7" templateTitle="Tranquility" templateImg={tranquility}></InvoiceTemplate>
-							<InvoiceTemplate templateId="7" templateTitle="Modernist" templateImg={modernist}></InvoiceTemplate>
+							<InvoiceTemplate templateId="7" templateTitle="Modernist" templateImg={modernist}></InvoiceTemplate> */}
 							
 						</CardBody>
 					</Card>

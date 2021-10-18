06ae8aac185ee9c9640d451ac788ba45e66ccd4a
Suraj Rahade <Suraj.Rahade@datainn.io>
2021-10-18 17:44:48 +0530

    1573,1583,1633,1634

--- src/screens/salaryStructure/screens/create/screen.js
+++ src/screens/salaryStructure/screens/create/screen.js
@@ -182,6 +182,7 @@ class CreateSalaryStructure extends React.Component {
                                   <Label htmlFor="select"><span className="text-danger">*</span> {strings.SalaryStructureType}</Label>
                                   <Input
                                     type="text"
+                                    maxLength="30"
                                     id="type"
                                     name="type"
                                     value={props.values.type}
@@ -201,6 +202,7 @@ class CreateSalaryStructure extends React.Component {
                                   <Label htmlFor="select"><span className="text-danger">*</span>{strings.SalaryStructureName}</Label>
                                   <Input
                                     type="text"
+                                    maxLength="30"
                                     id="name"
                                     name="name"
                                     value={props.values.name}

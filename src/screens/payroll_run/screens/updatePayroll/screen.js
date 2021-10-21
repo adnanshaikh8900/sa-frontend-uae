286881768c0feb9b63a13d798da25d06894df978
datainn <suraj.rahade@datainn.io>
2021-10-21 22:47:31 +0530

    update

--- src/screens/payroll_run/screens/updatePayroll/screen.js
+++ src/screens/payroll_run/screens/updatePayroll/screen.js
@@ -875,7 +875,7 @@ class UpdatePayroll extends React.Component {
 										<Col lg={12}>
 											<div className="h4 mb-0 d-flex align-items-center">
 												<i className="nav-icon fas fa-user-tie" />
-												<span className="ml-2">Update </span>
+												<span className="ml-2">Update Payroll</span>
 											</div>
 										</Col>
 									</Row>

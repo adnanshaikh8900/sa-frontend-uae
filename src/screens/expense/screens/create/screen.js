13913ab91117a1c80192bb132871c301b3f7c028
shoaib farooqui <suraj.rahade@datainn.io>
2021-12-06 15:14:54 +0530

    Limitations in Expense Bug no. 1989

--- src/screens/expense/screens/create/screen.js
+++ src/screens/expense/screens/create/screen.js
@@ -1190,7 +1190,7 @@ min="0"
 																			</Label>
 																			<Input
 																				type="text"
-																				maxLength="50"
+																				maxLength="20"
 																				id="receiptNumber"
 																				name="receiptNumber"
 																				placeholder={strings.Enter+strings.ReceiptNumber}

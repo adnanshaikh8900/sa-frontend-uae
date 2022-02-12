76fd0322db40b5f08a00838d30723f6c95b8c41a
shoaib farooqui <suraj.rahade@datainn.io>
2022-02-12 19:02:13 +0530

    date

--- src/screens/financial_report/sections/vat_reports/sections/generateVatReportModal.js
+++ src/screens/financial_report/sections/vat_reports/sections/generateVatReportModal.js
@@ -151,8 +151,8 @@ class GenerateVatReportModal extends React.Component {
 		const postData = {
 			// startDate: moment(this.state.initValue.startDate).format('DD/MM/YYYY'),
 			// endDate: moment(this.state.initValue.endDate).format('DD/MM/YYYY'),
-			startDate:this.getStartDate().replaceAll("/","-"),
-			endDate:this.getEndDate().replaceAll("/","-")
+			startDate:this.getStartDate().replaceAll("-","/"),
+			endDate:this.getEndDate().replaceAll("-","/")
 		};
 
 		this.props.vatReportActions

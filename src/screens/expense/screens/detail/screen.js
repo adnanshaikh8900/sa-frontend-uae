311b83b77abf594a88478d1f9a33b9b3252c87e4
shoaib farooqui <shoaib.farooqui@datainn.io>
2022-05-16 16:21:07 +0530

    Expense Type

--- src/screens/expense/screens/detail/screen.js
+++ src/screens/expense/screens/detail/screen.js
@@ -1038,7 +1038,7 @@ class DetailExpense extends React.Component {
 																</FormGroup>
 															</Col>)}
 
-															<Col className='mb-4' lg={3}>
+															<Col className='mb-4' lg={4}>
 															<Label htmlFor="inline-radio3"><span className="text-danger">* </span>{strings.ExpenseType}</Label>
 															<div>
 																{this.state.expenseType === false ?

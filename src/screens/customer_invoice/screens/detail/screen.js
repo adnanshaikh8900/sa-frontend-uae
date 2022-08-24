a1a110356d212ea22262bbc8d7eff875ef86380b
ajwatanvver <ajwa.tanveer@datainn.io>
2022-08-25 01:43:06 +0500

    Ticket no 259

--- src/screens/customer_invoice/screens/detail/screen.js
+++ src/screens/customer_invoice/screens/detail/screen.js
@@ -1793,8 +1793,8 @@ class DetailCustomerInvoice extends React.Component {
 																if (values.shippingPostZipCode == '')
 																	errors.shippingPostZipCode = 'Postal code is required';
 																else
-																	if (values.shippingPostZipCode.length != 6)
-																		errors.shippingPostZipCode = "Please enter 6 digit postal zip code"
+																	if (values.shippingPostZipCode.length < 3)
+																		errors.shippingPostZipCode = "Please enter atleast 3 digit postal zip code"
 															}}
 															return errors;
 													}}

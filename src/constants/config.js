511257687a93f1d4bae7f722fc12f00671d45a7c
nanak seth <nanak.seth@datainn.io>
2022-09-07 15:24:56 +0000

    Config Update

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,11 +1,13 @@
 export default {
-   API_ROOT_URL: 'https://dev-api.app.simpleaccounts.io',
+   //API_ROOT_URL: 'https://dev-api.app.simpleaccounts.io',
     // API_ROOT_URL: 'https://k8s-api.dev.simplevat.com',
    // API_ROOT_URL: 'http://localhost:8080',
     //  API_ROOT_URL: 'http://192.168.2.105:8080',
 
-  //  API_ROOT_URL: window._env_.SIMPLEVAT_HOST,
-  //  FRONTEND_RELEASE: window._env_.SIMPLEVAT_RELEASE,
+    API_ROOT_URL: window._env_.SIMPLEVAT_HOST,
+    FRONTEND_RELEASE: window._env_.SIMPLEVAT_RELEASE,
+  
+  
   DASHBOARD:true,
   INCOME_MODULE:true,
   EXPENSE_MODULE:true,

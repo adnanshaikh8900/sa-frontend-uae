08156329c7dd6c0412a3fa931758348efbac5be0
ikrama1524 <113528524+ikrama1524@users.noreply.github.com>
2023-02-14 16:13:42 +0530

    Excise Checkbox changes done by Ikrama shaikh 14/2/2023

--- src/screens/product/screens/detail/screen.js
+++ src/screens/product/screens/detail/screen.js
@@ -1408,7 +1408,7 @@ renderName=(cell,row)=>{
 															
 															<Row style={{display: props.values.productType !='SERVICE'   ?'' : 'none'}}		>
 																{this.state.companyDetails && this.state.companyDetails.isRegisteredVat===true &&(<Col lg={4}>
-																<FormGroup check inline className="mb-3">
+																<FormGroup check inline className="mb-3" hidden={this.state.childRecordsPresent}>
 																		<Label
 																			className="form-check-label"
 																			check
@@ -1416,7 +1416,7 @@ renderName=(cell,row)=>{
 																		>
 																			
 																			<Input
-																				disabled={this.state.childRecordsPresent}
+																				// disabled={this.state.childRecordsPresent}
 																				type="checkbox"
 																				id="exciseTaxCheck"
 																				name="exciseTaxCheck"

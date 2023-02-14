6839c96a51d9da6458e2408891c90f2e453492f9
Shoaib Farooqui <shoaib.farooqui@datainn.io>
2023-02-14 10:39:13 +0000

    COA action.js

--- src/screens/chart_account/screens/detail/actions.js
+++ src/screens/chart_account/screens/detail/actions.js
@@ -48,3 +48,20 @@ export const deleteChartAccount = (id) => {
     })
   }
 }
+export const checkValidation = (obj) => {
+	return (dispatch) => {
+		let data = {
+			method: 'get',
+			url: `/rest/validation/validate?name=${obj.name}&moduleType=${obj.moduleType}`,
+		};
+		return authApi(data)
+			.then((res) => {
+				if (res.status === 200) {
+					return res;
+				}
+			})
+			.catch((err) => {
+				throw err;
+			});
+	};
+};

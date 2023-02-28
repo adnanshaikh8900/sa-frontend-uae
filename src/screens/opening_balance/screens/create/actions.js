c38e257ecfcf1ae713a843306b8b760dabb42313
ajwatanvver <ajwa.tanveer@datainn.io>
2023-02-28 18:51:59 +0500

    ticket 287 PROD

--- src/screens/opening_balance/screens/create/actions.js
+++ src/screens/opening_balance/screens/create/actions.js
@@ -1,3 +1,4 @@
+import { OPENING_BALANCE } from 'constants/types'
 import {
   authApi
 } from 'utils'
@@ -11,6 +12,20 @@ export const addOpeningBalance = (obj) => {
       method: 'post',
       url:`/rest/transactionCategoryBalance/save`,
       data: obj
+    }
+    return authApi(data).then((res) => {
+      return res
+    }).catch((err) => {
+      throw err
+    })
+  }
+}
+export const getOpeningBalanceList = () => {
+  let param = `/rest/transactionCategoryBalance/list?paginationDisable=true`;
+  return (dispatch) => {
+    let data = {
+      method: 'GET',
+      url: param
     }
     return authApi(data).then((res) => {
       return res

87cf064d012b3cb7a90ab52c75f08a13f9f65a97
Taranuum786 <fazil616shaikh@gmail.com>
2023-09-15 17:40:01 +0530

    Ticket No.1650 done by Shaikh Zeba on 15/9/2023

--- src/screens/payroll_configurations/actions.js
+++ src/screens/payroll_configurations/actions.js
@@ -81,17 +81,3 @@ import {
 // 	  })
 // 	}
 //   }
-export const getCompanyById = () => {
-	return (dispatch) => {
-		let data = {
-			method: 'GET',
-			url: `/rest/company/getCompanyDetails`
-		}
-
-		return authApi(data).then((res) => {
-			return res
-		}).catch((err) => {
-			throw err
-		})
-	}
-}

00afd570bf8d18e40a83348496eb97f666ffc057
Fazil <fazil616shaikh@gmail.com>
2023-09-22 00:01:40 +0530

    Ticket Number: 1802 Payroll Genrator > Reason for rejecting the payroll Field Display Before Rejected

--- src/screens/payroll_run/screens/approver/screen.js
+++ src/screens/payroll_run/screens/approver/screen.js
@@ -1080,6 +1080,7 @@ class PayrollApproverScreen extends React.Component {
 																			<FormGroup>
 																				{this.state.status && (this.state.status === "Partially Paid" || this.state.status === "Paid" || this.state.status === "Draft") ?
 																					'' : (
+																						this.state.status &&( this.state.status === "Submitted" || this.state.status === "Rejected" )&& this.props.location?.state?.user !== 'Generator' &&
 																						<div>
 
 																							<Label htmlFor="payrollSubject">

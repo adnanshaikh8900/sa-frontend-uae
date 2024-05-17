a40e0d55f63cfdc863cadeabf7b064192cbe8504
kabir7378 <kabir7378@github.com>
2024-05-17 18:59:15 +0530

    Added validation for Invoice amount and explained transaction amount (Ticket #2624)

--- src/screens/bank_account/screens/transactions/sections/explain_transaction_detail.js
+++ src/screens/bank_account/screens/transactions/sections/explain_transaction_detail.js
@@ -1635,7 +1635,7 @@ class ExplainTrasactionDetail extends React.Component {
                                 });
                                 if (isExplainAmountZero) {
                                   errors.invoiceIdList =
-                                    "Expain Amount Cannot Be Zero";
+                                    "Explained Amount Cannot Be Zero";
                                 }
                               }
 
@@ -1674,6 +1674,7 @@ class ExplainTrasactionDetail extends React.Component {
                               ) {
                                 errors.amount = `The transaction amount is less than the invoice amount. To partially pay the invoice, please select the checkbox `;
                               }
+                              return errors;
                             }
 
                             if (
@@ -4244,7 +4245,16 @@ class ExplainTrasactionDetail extends React.Component {
                                               type="button"
                                               color="primary"
                                               className="btn-square mr-3"
-                                              onClick={props.handleSubmit}
+                                              onClick={() => {
+                                                    //	added validation popup	msg
+                                                    props.handleBlur();
+                                                    if (
+                                                      props.errors &&
+                                                      Object.keys(props.errors).length != 0
+                                                    )
+                                                      this.props.commonActions.fillManDatoryDetails();
+                                                    this.setState(props.handleSubmit())
+                                              }}
                                             >
                                               <i className="fa fa-dot-circle-o"></i>{" "}
                                               {strings.Explain}

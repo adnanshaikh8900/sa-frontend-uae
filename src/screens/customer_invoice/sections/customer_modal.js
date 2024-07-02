2b3c8569dcc5cded00574eaac48e9c30bc261bdf
ShaikhJ18 <J.shk1980@gmail.com>
2024-07-02 00:34:05 -0400

    fixed Confirmation Message for Quotations, Customer Invoice, and Supplier Invoice

--- src/screens/customer_invoice/sections/customer_modal.js
+++ src/screens/customer_invoice/sections/customer_modal.js
@@ -11,109 +11,12 @@ class CustomerModal extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			language: window['localStorage'].getItem('language'),
-			showDetails : false,
-			loading: false,
 			initValue: {
-				contactType: 2,
-				billingEmail: '',
-				city: '',
-				contractPoNumber: '',
-				countryId: '',
-				currencyCode: '',
-				email: '',
-				firstName: '',
-				addressLine1: '',
-				addressLine2: '',
-				addressLine3: '',
-				// language(Language, optional),
-				lastName: '',
-				middleName: '',
-				mobileNumber: '',
-				organizationName: '',
-			    poBoxNumber: '',
-				postZipCode: '',
-				stateId: '',
-				telephone: '',
-				vatRegistrationNumber: '',
-				disabled: false,
-				checkmobileNumberParam:false,
 				showConfirmation: false, 
 			},
 			state_list: [],
 		};
-		this.formikRef = React.createRef();
-		this.regEx = /^[0-9\d]+$/;
-		this.regExBoth = /[a-zA-Z0-9]+$/;
-		this.regExAlpha = /^[a-zA-Z ]+$/;
-		this.regExAddress = /^[a-zA-Z0-9\s,'-]+$/;
 	}
-
-	getData = (data) => {
-		let temp = {};
-		for (let item in data) {
-			if (typeof data[`${item}`] !== 'object') {
-				temp[`${item}`] = data[`${item}`];
-			} else {
-				temp[`${item}`] = data[`${item}`].value;
-			}
-		}
-		return temp;
-	};
-
-	// Create or Contact
-	handleSubmit = (data, resetForm, setSubmitting) => {
-		this.setState({ disabled: true });
-		const postData = this.getData(data);
-		this.props
-			.createCustomer(postData)
-			.then((res) => {
-				let resConfig = JSON.parse(res.config.data);
-				
-				if (res.status === 200) {
-					this.setState({ disabled: false });
-					resetForm();
-					this.props.closeCustomerModal(true);
-
-					let tmpData = res.data;
-					tmpData.currencyCode = resConfig.currencyCode;
-
-					this.props.getCurrentUser(tmpData);
-				}
-			})
-			.catch((err) => {
-				this.setState({ disabled: false });
-				this.displayMsg(err);
-				this.formikRef.current.setSubmitting(false);
-			});
-	};
-
-	displayMsg = (err) => {
-		toast.error(`${err.data.message}`, {
-			position: toast.POSITION.TOP_RIGHT,
-		});
-	};
-	_showDetails = (bool) => {
-		this.setState({
-		  showDetails: bool
-		});
-	  }
-
-	getStateList = (countryCode) => {
-		if (countryCode) {
-			this.props.getStateList(countryCode).then((res) => {
-				if (res.status === 200) {
-					this.setState({
-						state_list: res.data,
-					});
-				}
-			});
-		} else {
-			this.setState({
-				state_list: [],
-			});
-		}
-	};
 	openConfirmation = () => {
         this.setState({ showConfirmation: true });
     };
@@ -130,7 +33,7 @@ class CustomerModal extends React.Component {
     };
 
 	render() {
-		strings.setLanguage(this.state.language);
+		strings.setLanguage(window['localStorage'].getItem('language'));
 		const {
 			openCustomerModal,
 			closeCustomerModal,

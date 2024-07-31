dba6fb91bde61f4f7091a6628330a41439631552
Noumaan <Noumaan.khan@datainn.io>
2024-07-31 16:44:37 +0530

    CSS correction for pdf reports (Done by Noumaan)

--- src/screens/financial_report/sections/sales_by_product/screen.js
+++ src/screens/financial_report/sections/sales_by_product/screen.js
@@ -334,8 +334,8 @@ exportExcelFile = () => {
               <CardBody id="section-to-print">
                 <PDFExport
                   ref={(component) => (this.pdfExportComponent = component)}
-                  scale={0.454}
-                  paperSize="A3"
+                  scale={1}
+                  paperSize="auto"
                   fileName="Sales By Product.pdf"
                   margin={{top:0 , bottom:0 , left: 30 , right: 31 }}
                 >
